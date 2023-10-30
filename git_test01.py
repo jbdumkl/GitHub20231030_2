fdfd806349937dbe0f04934210f33bd60a6c63a3
jbdmkl <gglmkl@gmail.com>
2023-10-30 12:04:28 +0900

    sum function & numpy

--- git_test01.py
+++ git_test01.py
@@ -5,3 +5,12 @@ print(sum(3,5))
 sum2 = lambda a,b: a/b
 print(sum2(3,5))
 
+import numpy as np
+print(np.__version__)
+
+ar4 = [1,2,3,4,5,6]
+print(ar4)
+ar4 = np.array([1,2,3,4,5,6])
+print(ar4)
+ar4 = np.array([1,2,3,4,5,6]).reshape((3,2))
+print(ar4)
